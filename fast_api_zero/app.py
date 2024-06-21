45ffb0dad7c05354d76d36006158e77c9b0741ed
Henrique Nery <henriquenery07@gmail.com>
2024-06-20 22:56:49 -0300

    feat: adiciona rota e logica para adicionar usuário

--- fast_api_zero/app.py
+++ fast_api_zero/app.py
@@ -2,11 +2,22 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_api_zero.schemas import Message, UserSchema
+from fast_api_zero.schemas import Message, UserDB, UserPublic, UserSchema
+
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello world!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+
+    return user
--- fast_api_zero/schemas.py
+++ fast_api_zero/schemas.py
@@ -4,7 +4,17 @@ from pydantic import BaseModel, EmailStr
 class Message(BaseModel):
     message: str
 
+
 class UserSchema(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class UserDB(UserSchema):
+    id: int
